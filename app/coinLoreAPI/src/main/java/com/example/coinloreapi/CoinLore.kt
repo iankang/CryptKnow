7b226c1e9db049f987e033d8589737401c857ac3
ianngech <ianngech@gmail.com>
2022-03-12 18:50:07 +0300

    coinLoreAPI standalone for koin initialization

--- app/coinLoreAPI/src/main/java/com/example/coinloreapi/CoinLore.kt
+++ app/coinLoreAPI/src/main/java/com/example/coinloreapi/CoinLore.kt
@@ -1,20 +1,23 @@
-package com.example.cryptknow
+package com.example.coinloreapi
 
 import android.app.Application
+import com.example.coinloreapi.di.coinLoreModule
+import com.example.coinloreapi.di.repositoryModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
 
-class CryptKnow:Application() {
-
+class CoinLore:Application() {
     override fun onCreate() {
         super.onCreate()
-
         startKoin{
             androidLogger(Level.DEBUG)
             androidContext(applicationContext)
-            modules()
+            modules(listOf(
+                coinLoreModule,
+                repositoryModule
+            ))
         }
     }
 }
